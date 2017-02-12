81082297edaa7f65f5fe93012bd473a925bf8c91
Kyohei Mizutani <kyohei.mizutani@gmail.com>
2017-02-12 21:10:27 +0900

    feat: Add warp zone and character.

---
+++ Assets/MyAssets/Scripts/EnterWarp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterWarp : MonoBehaviour {
+
+	public string nextScene;
+	public bool flg = false;
+
+	public GameObject Warp;
+
+	private Animator warp_animator;
+
+	void Start () {
+		warp_animator = Warp.GetComponent<Animator> ();
+	}
+	
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log ("Enter");
+		EnterEffect ();
+	}
+
+	void OnTriggerExit(Collider collider) {
+		Debug.Log ("Exit");
+		ExitEffect ();
+	}
+
+	public void EnterEffect() {
+		warp_animator.SetTrigger ("enterTrigger");
+	}
+
+	public void ExitEffect() {
+		warp_animator.SetTrigger ("exitTrigger");
+
+	}
+
+}
