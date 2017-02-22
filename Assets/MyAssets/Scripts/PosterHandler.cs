82f94f812333516149571eeed5faab33ef91b0f0
Kyohei Mizutani <kyohei.mizutani@gmail.com>
2017-02-22 19:31:18 +0900

    feat: Create posters from script.

---
+++ Assets/MyAssets/Scripts/PosterHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PosterHandler : MonoBehaviour {
+
+	public Text title;
+	public Text description;
+	public Image image;
+
+	public string title_text;
+	public string description_text;
+	public string image_name;
+
+	void Start () {
+		title.text = title_text;
+		description.text = description_text;
+		Texture2D texture = Resources.Load("Images/PosterImages/" + image_name) as Texture2D;
+		image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+	}
+}
+
+[Serializable]
+public class PosterHandlers {
+	public List<PosterHandler> list;
+}
