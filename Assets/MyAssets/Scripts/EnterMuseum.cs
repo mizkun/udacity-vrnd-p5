22748cac9e69a06cf0900b676e5e9f3992a3e842
Kyohei Mizutani <kyohei.mizutani@gmail.com>
2017-02-24 21:11:38 +0900

    feat: Smooth scene transition.

--- Assets/MyAssets/Scripts/EnterMuseum.cs
+++ Assets/MyAssets/Scripts/EnterMuseum.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
 
 public class EnterMuseum : MonoBehaviour {
 
 	public static int museum_id;
+	public static string museum_name;
 	
 	public GameObject character;
 	public float velocity = 30.0f;
+	public float fadeVelocity = 0.5f;
 	public int this_museum_id;
+	public string this_museum_name;
+	public Image Fadeout;
 
 	bool inCore;
 
@@ -23,6 +28,10 @@ public class EnterMuseum : MonoBehaviour {
 			character.GetComponent<HeadLookWalk> ().isWalking = false;
 			float newY = character.transform.position.y + moveY;
 			character.transform.position = new Vector3 (transform.position.x, newY, transform.position.z);
+
+			Color newColor = new Color (Fadeout.color.r, Fadeout.color.g, Fadeout.color.b, Fadeout.color.a + fadeVelocity * Time.deltaTime);
+			Fadeout.color = newColor;
+			Debug.Log (Fadeout.color);
 		}
 	}
 
@@ -30,15 +39,21 @@ public class EnterMuseum : MonoBehaviour {
 		Debug.Log ("EnterCore");
 		inCore = true;
 		museum_id = this_museum_id;
+		museum_name = this_museum_name;
 	}
 
 	void OnTriggerExit(Collider collider) {
 		Debug.Log ("ExitCore");
 		inCore = false;
-		SceneManager.LoadScene ("Museum");
+		CameraFade.StartAlphaFade(Color.white, false, 2.0f, 0.0f, () => {SceneManager.LoadScene ("Museum");});
+		//SceneManager.LoadScene ("Museum");
 	}
 
 	public static int getMuseumId () {
 		return museum_id;
+	}
+
+	public static string getMuseumName () {
+		return museum_name;
 	}
 }
--- Assets/MyAssets/Scripts/PosterCreator.cs
+++ Assets/MyAssets/Scripts/PosterCreator.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PosterCreator : MonoBehaviour {
 
 	private string sample_json = "{\"Items\":[\n  {\n    \"museum_id\": 1,\n    \"poster_id\": 1,\n    \"title_text\": \"Youtube 360\",\n    \"image_name\": \"poster_image_001\",\n    \"poster_url\": \"https://adwords.googleblog.com/2015/07/see-future-of-video-by-looking-behind.html\",\n    \"description_text\": \"360 degree video ads let you engage your audience in an entirely new way. Available today in Chrome and the YouTube app on Android and iOS, viewers can explore every angle of your videos by dragging their mouse or moving their phone to shift their POV 360 degrees – up, down, left or right. Thanks to the ability simply to move your phone to see different parts of the video, 360 video ads are a truly mobile-first video ad product.\"\n  },\n  {\n    \"museum_id\": 1,\n    \"poster_id\": 2,\n    \"title_text\": \"Immersv\",\n    \"image_name\": \"poster_image_002\",\n    \"poster_url\": \"http://venturebeat.com/2016/03/10/immersv-enables-virtual-reality-developers-to-advertise-their-games-in-other-vr-apps/\",\n    \"description_text\": \"Immersv is a VR advertising platform that wants to help gamemakers and app designers with discovery while also providing them with a way to generate revenue. The idea is to enable studios to insert video commercials into their VR experiences. The startup already launched its ad exchange in a closed beta for a few clients in December with support for mobile headsets like Samsung’s Gear VR and Google Cardboard.\"\n  },\n  {\n    \"museum_id\": 1,\n    \"poster_id\": 3,\n    \"title_text\": \"Vertebrae\",\n    \"image_name\": \"poster_image_003\",\n    \"poster_url\": \"http://www.vertebrae.io\",\n    \"description_text\": \"Vertebrae is a technology company building the future of Virtual Reality (VR) and Augmented Reality (AR) advertising.\\nOur mission is to make VR accessible to publishers, advertisers and consumers alike through our next generation monetization platform. Our headset-agnostic VR ad tech is currently in private beta and is being used by entertainment studios, gaming companies, brands and creative agencies to tell stories in dynamic virtual environments. Out technology enables native 3D and 360° video ad insertion within a range of fully immersive and interactive ad formats.\\n\\nThe Vertebrae team is comprised of creative professionals from new media, gaming, and advertising from companies like Oculus, Google, Activision. and Twitter. We are passionately innovating the future of VR and AR advertising.\\n\\nRaised $10M in Sept 2016.\"\n  },\n  {\n    \"museum_id\": 2,\n    \"poster_id\": 4,\n    \"title_text\": \"CognitiveVR\",\n    \"image_name\": \"poster_image_004\",\n    \"poster_url\": \"http://cognitivevr.co\",\n    \"description_text\": \"CognitiveVR is a brand new analytics solution that overcomes the complexity of virtual reality (VR) to bring you deep insights into how your users are interacting with your products. Our technology is hardware agnostic, developer friendly, and easy to configure. Open APIs mean less time onboarding and more time building your VR products.\"\n  },\n  {\n    \"museum_id\": 2,\n    \"poster_id\": 5,\n    \"title_text\": \"Retinad\",\n    \"image_name\": \"poster_image_005\",\n    \"poster_url\": \"http://retinadvr.com\",\n    \"description_text\": \"The World's First Ads and Analytics Platform made exclusively for Virtual Reality Developers\\n\\nRaised a $500k Angel round in May 2015. In River 2\"\n  }\n]}";
 	int museum_id;
+	string museum_name;
 
+	public Text museumTitle;
 	public GameObject posterObject;
-
+	public Image Fadein;
+	public float fadeVelocity = 0.5f;
 
 	void Awake () {
 		museum_id = EnterMuseum.getMuseumId ();
+		museum_name = EnterMuseum.getMuseumName () + " Museum";
 		Debug.Log (museum_id);
 	}
 
 	void Start () {
 		Poster[] posters = JsonHelper.FromJson<Poster>(sample_json);
+		museumTitle.text = museum_name;
 
 		foreach (Poster p in posters) {
 			if (p.museum_id == museum_id) {
@@ -27,7 +33,10 @@ public class PosterCreator : MonoBehaviour {
 	}
 
 	void Update () {
-		
+		if (Fadein.color.a > 0.0f) {
+			Color newColor = new Color (Fadein.color.r, Fadein.color.g, Fadein.color.b, Fadein.color.a - fadeVelocity * Time.deltaTime);
+			Fadein.color = newColor;
+		}
 	}
 
 	void CreatePoster (string title, string description, string image_name) {
