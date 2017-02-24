6b24128f58157581cf2f7359ded67f492764941c
Kyohei Mizutani <kyohei.mizutani@gmail.com>
2017-02-24 19:31:59 +0900

    feat: Add back to home feature and tutorial in Museum Scene.

---
+++ Assets/MyAssets/Scripts/BackToHome.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
+
+public class BackToHome : MonoBehaviour {
+
+	public Image progress;
+	public float speed;
+	public GameObject character;
+
+	private bool isEnter;
+	
+	void Start () {
+		isEnter = false;
+		progress.fillAmount = 0.0f;
+	}
+	
+	void Update () {
+		if (isEnter) {
+			progress.fillAmount += speed * Time.deltaTime;
+			if (progress.fillAmount >= 1.0f) {
+				Back ();
+			}
+		}
+	}
+
+	public void EnterBackButton () {
+		Debug.Log ("Enter Back Button");
+		isEnter = true;
+		character.GetComponent<HeadLookUpDown> ().isWalking = false;
+	}
+
+	public void ExitBackBButton () {
+		Debug.Log ("Exit Back Button");
+		isEnter = false;
+		progress.fillAmount = 0.0f;
+		character.GetComponent<HeadLookUpDown> ().isWalking = true;
+	}
+
+	public void Back () {
+		SceneManager.LoadScene ("Start");
+	}
+}
--- Assets/MyAssets/Scripts/HeadLookUpDown.cs
+++ Assets/MyAssets/Scripts/HeadLookUpDown.cs
@@ -6,18 +6,28 @@ public class HeadLookUpDown : MonoBehaviour {
 
 	public float velocity = 0.7f;
 	public float minimumVelocity = 0.002f;
+	public bool isWalking;
+	public GameObject Tutorial;
+	public GameObject BackToHome;
 
-	// Use this for initialization
+
 	void Start () {
-		
+		isWalking = true;
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		float moveY = Camera.main.transform.forward.y * velocity * Time.deltaTime;
-		if (Mathf.Abs (moveY) > minimumVelocity) {
-			float newY = transform.position.y + moveY;
+		float newY = transform.position.y + moveY;
+
+		if (Mathf.Abs (moveY) > minimumVelocity && isWalking) {
 			transform.position = new Vector3 (transform.position.x, newY, transform.position.z);
+		}
+		if (newY < -1.0f) {
+			Tutorial.SetActive (false);
+			BackToHome.SetActive (true);
+		} else {
+			Tutorial.SetActive (true);
+			BackToHome.SetActive (false);
 		}
 	}
 }
