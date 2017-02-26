942c866a46efbb24418b9907d8e4ab1cdcc47c69
Kyohei Mizutani <kyohei.mizutani@gmail.com>
2017-02-26 13:49:51 +0900

    feat: Add tutorial.

---
+++ Assets/MyAssets/Scripts/ExitTutorial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitTutorial : MonoBehaviour {
+
+	public GameObject tutorial;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log ("Enter");
+		tutorial.SetActive (true);
+	}
+
+	void OnTriggerExit(Collider collider) {
+		Debug.Log ("Exit");
+		tutorial.SetActive (false);
+	}
+}
+
+
